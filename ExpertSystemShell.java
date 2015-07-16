898370d80bb945387f08e23a6c72a9d24b8c5bfb
TJHockeyKid <TJHockeyKid@users.noreply.github.com>
2015-07-16 08:09:09 -0400

    Update ExpertSystemShell.java

--- ExpertSystemShell.java
+++ ExpertSystemShell.java
@@ -69,3 +69,59 @@ public class ExpertSystemShell {
 		}
 	}
 }
+
+
+class TreeNode
+{
+   private Object value; 
+   private ArrayList<TreeNode> children;
+   
+   public TreeNode(Object initValue)
+   { 
+      value = initValue; 
+      ArrayList<TreeNode> children = new ArrayList<TreeNode>(); 
+   }
+   
+   public TreeNode(Object initValue, TreeNode initLeft, TreeNode initRight)
+   { 
+      value = initValue; 
+      children.add(initLeft);
+      children.add(initRight);
+   }
+   
+   public TreeNode(Object initValue, TreeNode initSingle)
+   { 
+      value = initValue; 
+      children.add(initSingle);
+   }
+   
+   public Object getValue()
+   { 
+      return value; 
+   }
+   
+   public TreeNode getLeft() 
+   { 
+      return children.get(0); 
+   }
+   
+   public TreeNode getRight() 
+   { 
+      return children.get(1); 
+   }
+   
+   public void setValue(Object theNewValue) 
+   { 
+      value = theNewValue; 
+   }
+   
+   public void setLeft(TreeNode theNewLeft) 
+   { 
+      children.set(0, theNewLeft);
+   }
+   
+   public void setRight(TreeNode theNewRight)
+   { 
+      children.set(1, theNewRight);
+   }
+}
